79214f49e6201f38253c624b05f067fadddf5e22
Stefan Siprell <stefan_siprell@epam.com>
2025-04-11 14:49:23 +0200

    Added first Jest Test (is actually an integration test)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,10 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.test.js"], // Apply Jest settings to test files
+    plugins: { jest: require("eslint-plugin-jest") },
+    extends: ["plugin:jest/recommended"],
+    languageOptions: { globals: globals.jest }
+  }
+
 ]);
