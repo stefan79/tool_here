966ff57e3d1f879c1aa2b7d818576725c6b5c2c1
Stefan Siprell <stefan.siprell@gmail.com>
2025-04-17 12:56:06 +0200

    4 add basic unit text coverage (#5)
    * Added first Jest Test (is actually an integration test)
    
    * First Draft
    
    * Updated Controller
    
    ---------
    
    Co-authored-by: Stefan Siprell <stefan_siprell@epam.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,16 @@
-import { defineConfig } from "eslint/config";
-import js from "@eslint/js";
-import globals from "globals";
+const { defineConfig } = require( "eslint/config");
+const js = require("@eslint/js");
+const globals = require( "globals");
 
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.test.js"], // Apply Jest settings to test files
+    plugins: { jest: require("eslint-plugin-jest") },
+    extends: ["plugin:jest/recommended"],
+    languageOptions: { globals: globals.jest }
+  }
+
 ]);
--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,38 @@
  */
 
 const express = require('express');
-const geocodeRoutes = require('./geocode.routes');
-const healthRoutes = require('./health.routes');
-const discoverRoutes = require('./discover.routes');
+const {geoRoutes} = require('./geocode.routes');
+const {healthRoutes} = require('./health.routes');
+const {discoverRoutes} = require('./discover.routes');
+const {mcpRoutes} = require('./mcp.routes')
 
-const router = express.Router();
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
-// Register all route modules
-router.use('/discover', discoverRoutes); 
-router.use('/geocode', geocodeRoutes);
-router.use('/health', healthRoutes);
+// Apply rate limiting
+const limiter = (config, client, server) => rateLimit({
+    windowMs: config.RATE_LIMIT.WINDOW_MS,
+    max: config.RATE_LIMIT.MAX_REQUESTS,
+    standardHeaders: true,
+    legacyHeaders: false,
+  });
 
-module.exports = router;
+module.exports = (app, config, client, server) => {
+    const router = express.Router();
+
+    // Register all route modules
+
+    app.use('/rest/',express.json());
+    app.use('/rest/',cors());
+    app.use('/rest/',helmet());
+    app.use('/rest/',limiter(config, client, server));
+
+
+    router.use('/rest/discover', discoverRoutes(config, client, server)); 
+    router.use('/rest/geocode', geoRoutes(config, client, server));
+    router.use('/rest/health', healthRoutes(config, client, server));
+    router.use('/', mcpRoutes(config, client, server));
+
+    return router;
+};
